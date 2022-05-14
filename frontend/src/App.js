c411b2888c8ca708f1043e5fe540bcdee85d2a4b
tseela <tseelagamzo@gmail.com>
2022-05-14 18:37:30 +0300

    connected users can open several windows without having to enter credentials again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home, Login } from "./pages"
+import { UseToken } from "./components"
 
 export default function App() {
-  const [token, setToken] = useState();
+  const { token, setToken } = UseToken();
+
   if(!token) {
     return <Login setToken={setToken} />
   }
