d64de42638e1a93178d8a4a57f2e91752bd58ffe
tseela <tseelagamzo@gmail.com>
2022-05-14 18:09:12 +0300

    created backend for testings and binded real backend to port 5000

---
+++ frontend/proxy.py
@@ -0,0 +1,39 @@
+# test server to use as backend for fronntend testings
+
+from flask import Flask , request, jsonify,make_response, render_template
+import jwt
+from datetime import datetime, timedelta
+
+# server
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '03a5t89c1pf9Uc0a0f7E'
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    return signUp(request.json['username'], request.json['password'])
+
+
+def signUp(username, password):
+    return signIn(username, password)
+
+
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    return signIn(request.json['username'], request.json['password'])
+
+
+def signIn(username, password):
+    token = jwt.encode({
+        'user' : username,
+        'expiration' : str(datetime.utcnow() + timedelta(seconds=120))
+    },
+    app.config['SECRET_KEY'])
+
+    return jsonify({'token' : token.decode('utf-8')})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
