1f084fc5f8af1a83cdb234a90c7d4c557f064d4b
tseela <tseelagamzo@gmail.com>
2022-05-28 17:04:08 +0300

    Added 404 message, altered elements size to depend on browser size and chaged logout to work from navbar

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -5,12 +5,12 @@ import {Navbar} from "../../components"
 export default function Home() {
     return (
         <main className='home'>
-            <header className='header'>
-            <Navbar />
-                <div className="page">
-                    hii
-                </div>
+            <header className='navbar'>
+                <Navbar />
             </header>
+            <div className="page">
+                hii
+            </div>
         </main>
     )
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,5 @@ export { default as Home } from './home/Home';
 export { default as Login } from './login/Login';
 export { default as SignUp } from './signup/SignUp';
 export { default as Profile } from './profile/Profile';
+export { default as PageNotFound } from './404/PageNotFound';
+
