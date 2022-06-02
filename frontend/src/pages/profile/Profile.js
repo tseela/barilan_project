298ffc03b2cd26b3731977f72614922ca2247a88
tseela <tseelagamzo@gmail.com>
2022-06-02 19:10:25 +0300

    edited 404 page to fit window size and created a list display for trips in profile page

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -4,24 +4,34 @@ import { useToken } from '../../hooks';
 
 export default function Profile() {
     const { token, setToken } = useToken();
+    let username = token[0]?.user;
+    const trips = [{id:1, name:"trip1"}, {id:2, name:"trip2"}, {id:3, name:"trip3"}];
+    const listItems = trips.map((trip) => <li key={trip.id}>{trip.name}</li>);
 
-    if (!token) { //alert("You need to log in first!") and move straight to /signin
+    if (!token) {
         return(
-            <main className='profile'>
+            <main className='profile-page'>
                 <header className='navbar'>
                     <Navbar />
                 </header>
-                <div className='body'>You need to log in first!<br></br><a href='/login'>Login</a></div>
+                <div className='connect body'>You need to log in first!<br></br><a href='/login'>Login</a></div>
             </main>
         );
     }
 
     return (
-        <main className='profile'>
+        <main className='profile-page'>
             <header className='navbar'>
                 <Navbar />
             </header>
-            <div className='body'>replace with user profile</div>
+            <div className='profile body'>
+                <div className='profile-username'>
+                    {username}
+                </div>
+                <div className='profile-trips'>
+                    <ul>{listItems}</ul>
+                </div>
+            </div>
         </main>
     );
 }
