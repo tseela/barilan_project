a17d0789054bd68c539a4e320718f7a39269be23
tseela <tseelagamzo@gmail.com>
2022-06-17 15:48:31 +0300

    change /insertTrip to /insertTripToUser

--- frontend/src/pages/edittrip/EditTrip.js
+++ frontend/src/pages/edittrip/EditTrip.js
@@ -66,7 +66,7 @@ export default function EditTrip() {
             et.name = editedTripName;
         }
 
-        fetch('/insertTrip', {
+        fetch('/insertTripToUser', { // should by updateTrip
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
